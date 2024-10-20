61bf2cdb067f3ecf0e9d26f2250176596a6e834e
Juan Guillermo Mac <donsonydos@gmail.com>
2024-10-19 22:26:25 -0500

    Creación página customer location

--- src/app/visit/customer-location/customer-location.component.ts
+++ src/app/visit/customer-location/customer-location.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { FooterTwoComponent } from 'src/app/footer/footer-two/footer-two.component';
+import { FormsModule } from '@angular/forms';
+import { AutocompleteLibModule } from 'angular-ng-autocomplete';
+import { Router, RouterModule } from '@angular/router';
+import { GeneralFunctionsService } from 'src/app/services/general-functions.service';
+import { ApiService } from 'src/app/services/api.service';
+import { CustomerService } from 'src/app/services/customer.service';
 
 @Component({
   selector: 'app-customer-location',
   standalone: true,
-  imports: [FooterTwoComponent],
+  imports: [AutocompleteLibModule, FormsModule, FooterTwoComponent, RouterModule],
   templateUrl: './customer-location.component.html',
   styleUrl: './customer-location.component.scss'
 })
 export class CustomerLocationComponent {
+  public customerIdSelected: Number;
 
+  constructor(
+  private api: ApiService,
+  public customers: CustomerService,
+  private generalFunctions: GeneralFunctionsService,
+  private router: Router
+) {
+  this.customerIdSelected = 0;
+}
+
+onFocused(event) {
+  // console.log('evento', event);
+}
+
+ngOnInit() {
+  this.customers.getCustomers().then();
+  this.generalFunctions.getLocation().then((message) => console.info('responde', message));
+}
+
+saveLocation() {
+}
 }
