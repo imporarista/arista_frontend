d166385f0597696cd01ca87f0574973357f64063
Juan Guillermo Mac <donsonydos@gmail.com>
2024-10-23 23:33:30 -0500

    Detalles del pedido

--- src/app/visit/order-detail/order-detail.component.ts
+++ src/app/visit/order-detail/order-detail.component.ts
@@ -1,14 +1,14 @@
-import { isPlatformBrowser } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { Component, Inject, Input, PLATFORM_ID, TemplateRef, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/interfaces/product';
 import { ApiService } from 'src/app/services/api.service';
+import { Orderdetailinterface } from 'src/app/interfaces/orderdetailinterface';
 
 @Component({
   selector: 'app-order-detail',
   standalone: true,
-  imports: [],
+  imports: [NgIf, CommonModule],
   templateUrl: './order-detail.component.html',
   styleUrl: './order-detail.component.scss'
 })
@@ -21,81 +21,54 @@ export class OrderDetailComponent {
   public ImageSrc: string;
   public counter: number = 1;
   public modalOpen: boolean = false;
+  public detailOrder = {
+    subtotal: 0,
+    tax: 0,
+    total: 0
+  };
+  public productsOrder: Orderdetailinterface[] = [
+    {
+      // imagen principal del producto
+      image: '',
+      // nombre del producto o referencia
+      prod_name: '',
+      // descripción del producto
+      prod_descriptions: '',
+      // precio indivudual
+      price: 0,
+      // cantidad seleccionada
+      quantity: 0
+    }
+  ];
+  public currentTax = 0.19;
 
-  constructor(@Inject(PLATFORM_ID) 
-  private platformId: Object,
-    private router: Router, 
-    private modalService: NgbModal,
-    public apiService: ApiService
+
+  constructor(@Inject(PLATFORM_ID)
+    private router: Router,
+    public apiService: ApiService,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-  }
-
-  openModal() {
-    this.modalOpen = true;
-    if (isPlatformBrowser(this.platformId)) { // For SSR 
-      this.modalService.open(this.QuickView, {
-        size: 'lg',
-        ariaLabelledBy: 'modal-basic-title',
-        centered: true,
-        windowClass: 'Quickview'
-      }).result.then((result) => {
-        `Result ${result}`
-      }, (reason) => {
-        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    this.activatedRoute.params.subscribe((data: any) => {
+      this.apiService.getCustomerOrderDetail(data.orderId).subscribe((dataOrder: any) => {
+        this.productsOrder = dataOrder;
+        this.calculateTotalOrder();
       });
-    }
+    });
   }
 
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return `with: ${reason}`;
-    }
+  calculateTotalOrder() {
+    this.detailOrder.subtotal = 0;
+    this.productsOrder.forEach((product: any) => {
+      this.detailOrder.subtotal += (product.price * product.quantity);
+    });
+    this.detailOrder.tax = this.detailOrder.subtotal * this.currentTax;
+    this.detailOrder.total = this.detailOrder.subtotal + this.detailOrder.tax;
   }
 
-  // Get Product Color
-  Color(variants) {
-    const uniqColor = []
-    for (let i = 0; i < Object.keys(variants).length; i++) {
-      if (uniqColor.indexOf(variants[i].color) === -1 && variants[i].color) {
-        uniqColor.push(variants[i].color)
-      }
-    }
-    return uniqColor
-  }
 
-  // Get Product Size
-  Size(variants) {
-    const uniqSize = []
-    for (let i = 0; i < Object.keys(variants).length; i++) {
-      if (uniqSize.indexOf(variants[i].size) === -1 && variants[i].size) {
-        uniqSize.push(variants[i].size)
-      }
-    }
-    return uniqSize
-  }
-
-  // Change Variants
-  ChangeVariants(color, product) {
-    product.variants.map((item) => {
-      if (item.color === color) {
-        product.images.map((img) => {
-          if (img.image_id === item.image_id) {
-            this.ImageSrc = img.src
-          }
-        })
-      }
-    })
-  }
-
-  ngOnDestroy() {
-    if (this.modalOpen) {
-      this.modalService.dismissAll();
-    }
+  async openViewer(src: any) {
+    this.router.navigate(['/zoom', { src }]);
   }
 }
